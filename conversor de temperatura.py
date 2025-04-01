132d04f75e9b5d5567608d2f0614cfcc15f0ad85
Kaue Evaristo de Oliveira <kauecaqui@gmail.com>
2025-04-01 14:23:39 -0300

    conversor corrigido

--- conversor de temperatura.py
+++ conversor de temperatura.py
@@ -1,32 +1,29 @@
-celsius = 0
-fahrenheit = 0
-kelvin = 0
-
 print("----------------Conversor de Temperatura----------------")
-print("Qual temperatura que deseja converter? Celsius, Fahrenheit ou Kelvin")
+print("Qual temperatura que deseja converter? 1. Celsius, 2. Fahrenheit ou 3. Kelvin")
 print("--------------------------------------------------------")
 
-escolha = str(input("Escolha: "))
-if celsius:
-    celsius = float(input("Digite a temperatura em graus Celsius: "))
-    celsius = (fahrenheit - 32) * 5/9
-    celsius = (kelvin - 273.15)
-    print("Temperatura em Fahrenheit:", fahrenheit)
-    print("Temperatura em Kelvin:", kelvin)
+escolha = int(input("Escolha: "))
 
-elif escolha == fahrenheit:
-    fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
-    fahrenheit = (celsius * 9/5) + 32
-    fahrenheit = (kelvin - 273.15) * 9/5 + 32
-    print("Temperatura em Celsius:", celsius)
-    print("Temperatura em Kelvin:", kelvin)
+if escolha == 1: # Celsius
+    c = float(input("Digite a temperatura em graus Celsius: "))
+    f = c * 1.8 + 32
+    k = c + 273
+    print("Temperatura em Fahrenheit:", f)
+    print("Temperatura em Kelvin:", k)
 
-elif escolha == kelvin:
-    kelvin = float(input("Digite a temperatura em graus Kelvin: "))
-    kelvin = (celsius + 273.15)
-    kelvin = (fahrenheit - 32) * 5/9 + 273.15
-    print("Temperatura em Celsius:", celsius)
-    print("Temperatura em Fahrenheit:", fahrenheit)
+elif escolha == 2: # Fahrenheit
+    f = float(input("Digite a temperatura em graus Fahrenheit: "))
+    c = (f-32)/1.8
+    k = (f-32)*5/9 + 273
+    print("Temperatura em Celsius:", c)
+    print("Temperatura em Kelvin:", k)
+
+elif escolha == 3: # Kelvin
+    k = float(input("Digite a temperatura em graus Kelvin: "))
+    c = k - 273.15
+    f = (k - 273.15) * 9/5 + 32
+    print("Temperatura em Celsius:", c)
+    print("Temperatura em Fahrenheit:", f)
 
 else:
     print("Escolha inválida")
