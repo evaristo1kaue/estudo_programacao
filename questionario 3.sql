4c049feff7ec5a8fb5597c512746bc1b5cb60153
Kaue Evaristo de Oliveira <kauecaqui@gmail.com>
2025-04-02 16:38:12 -0300

    corrigido

--- questionario 3.sql
+++ questionario 3.sql
@@ -34,3 +34,20 @@ WHERE title like '%Love%'
 OR listed_in like '%Romance%'
 OR listed_in like '%Romantic%'
 AND type = 'Movie'
+
+--7
+SELECT country, COUNT(*) num_filmes
+FROM netflix_titles nt 
+where type = 'Movie'
+and country IS NOT NULL
+GROUP BY country
+HAVING COUNT(*) > 10
+ORDER BY num_filmes DESC
+
+--8
+SELECT listed_in, AVG(release_year) media_lancamento
+FROM netflix_titles nt
+where type = 'Movie'
+group by listed_in
+order by media_lancamento desc
+limit 3
